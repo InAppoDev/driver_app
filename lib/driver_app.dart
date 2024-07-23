672b6354e51011ecff1185cb68c38955886bed3f
b1gmax <shovkoplyasmaks02@gmail.com>
2024-07-23 16:53:58 +0300

    added localization

--- lib/driver_app.dart
+++ lib/driver_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tms_driver/presentation/theme/theme.dart';
 import 'router/app_router.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DriverApp extends StatelessWidget {
   const DriverApp({super.key});
@@ -13,6 +15,13 @@ class DriverApp extends StatelessWidget {
       routerDelegate: router.routerDelegate,
       routeInformationParser: router.routeInformationParser,
       routeInformationProvider: router.routeInformationProvider,
+      localizationsDelegates:  const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        ],
+      locale: const Locale('en'),
       supportedLocales: const [
         Locale('en', 'US'),
       ],
