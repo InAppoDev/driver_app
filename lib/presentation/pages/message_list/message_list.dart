f5d621e2ac19fcaa2057018253a233daa86a80d0
b1gmax <shovkoplyasmaks02@gmail.com>
2024-08-01 13:46:33 +0300

    fix

--- lib/presentation/pages/message_list/message_list.dart
+++ lib/presentation/pages/message_list/message_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tms_driver/presentation/blocks/message/message_bloc.dart';
-import 'package:tms_driver/presentation/pages/message_list/widget/message_widget.dart';
+import 'package:tms_driver/presentation/pages/message_list/widget/message_view.dart';
 
 class MessageListPage extends StatelessWidget {
   const MessageListPage({super.key});
@@ -10,30 +10,7 @@ class MessageListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => MessageBloc(),
-      child: BlocListener<MessageBloc, MessageState>(
-        listener: (context, listenerState) {
-          if (listenerState.status == MessageStatus.failure) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                  content:
-                      Text(listenerState.errorMessage ?? 'An error occurred')),
-            );
-          }
-        },
-        child: BlocBuilder<MessageBloc, MessageState>(
-          builder: (context, state) {
-            return Container(
-              decoration: BoxDecoration(color: Theme.of(context).canvasColor),
-              child: ListView.builder(
-                itemCount: 12,
-                itemBuilder: (context, index) {
-                  return const MessageWidget();
-                },
-              ),
-            );
-          },
-        ),
-      ),
+      child: const MessageView(),
     );
   }
 }
