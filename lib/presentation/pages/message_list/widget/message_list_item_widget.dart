e1c6af021a8714cbb0a93e37a46f0ac87066e1dd
b1gmax <shovkoplyasmaks02@gmail.com>
2024-08-27 11:53:43 +0300

    added unread messages

--- lib/presentation/pages/message_list/widget/message_list_item_widget.dart
+++ lib/presentation/pages/message_list/widget/message_list_item_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tms_driver/data/models/chats/chat/chat_model.dart';
 import 'package:tms_driver/presentation/pages/chat_detail/widget/chat_image.dart';
+import 'package:tms_driver/presentation/pages/message_list/widget/unread_count_widget.dart';
 
 class MessageListItemWidget extends StatelessWidget {
   final ChatModel chat;
@@ -15,8 +16,15 @@ class MessageListItemWidget extends StatelessWidget {
         context.push('/chat/${chat.id}');
       },
       child: Container(
-        color: Theme.of(context).canvasColor,
-        padding: const EdgeInsets.symmetric(horizontal: 13, vertical: 10),
+        margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 13),
+        padding: const EdgeInsets.symmetric(vertical: 11, horizontal: 6),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color:
+              chat.unreadMessagesCount != null && chat.unreadMessagesCount! > 0
+                  ? Theme.of(context).secondaryHeaderColor.withOpacity(0.2)
+                  : Theme.of(context).canvasColor,
+        ),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -33,31 +41,43 @@ class MessageListItemWidget extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    chat.title,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        chat.title,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Text(
+                        _formatTime(chat.lastUpdatedAt),
+                        style: const TextStyle(
+                          fontSize: 13,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
                   ),
                   const SizedBox(height: 5),
-                  Text(
-                    chat.lastMessage?.previewText ?? '',
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w300,
-                    ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        chat.lastMessage?.previewText ?? '',
+                        style: const TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w300,
+                        ),
+                      ),
+                      if (chat.unreadMessagesCount != null &&
+                          chat.unreadMessagesCount! > 0)
+                        UnreadCountWidget(count: chat.unreadMessagesCount!),
+                    ],
                   ),
                 ],
-              ),
-            ),
-            const SizedBox(width: 10),
-            Text(
-              _formatTime(chat.lastUpdatedAt),
-              style: const TextStyle(
-                fontSize: 13,
-                fontWeight: FontWeight.w400,
               ),
             ),
           ],
