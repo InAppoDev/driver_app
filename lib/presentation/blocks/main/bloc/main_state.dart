b7a9f5c2d7ab5ec590a3acc863fc5ab1d695650d
b1gmax <shovkoplyasmaks02@gmail.com>
2024-09-03 10:57:19 +0300

    fix

--- lib/presentation/blocks/main/bloc/main_state.dart
+++ lib/presentation/blocks/main/bloc/main_state.dart
@@ -7,12 +7,12 @@ class MainState with _$MainState {
   const factory MainState({
     required MainPageEnum selectedPage,
     required bool showNavBar,
-    required bool isConnected,
+    required bool isConnected, required bool  isDriveStarted,
   }) = _MainState;
 
   factory MainState.initial() => const MainState(
         selectedPage: MainPageEnum.home,
         showNavBar: true,
-        isConnected: true,
+        isConnected: true, isDriveStarted: false,
       );
 }
