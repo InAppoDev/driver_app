4cef858ec66c866ceb9a5caaa8f093a4aacbdf87
b1gmax <shovkoplyasmaks02@gmail.com>
2024-09-05 13:21:15 +0300

    fix

--- lib/presentation/pages/main/main_page.dart
+++ lib/presentation/pages/main/main_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tms_driver/presentation/blocks/main/bloc/main_bloc.dart';
 import 'package:tms_driver/presentation/blocks/message_list/message_list_bloc.dart';
+import 'package:tms_driver/presentation/blocks/notification/bloc/notification_bloc.dart';
 import 'package:tms_driver/presentation/blocks/trip_list/trip_bloc.dart';
 import 'package:tms_driver/presentation/blocks/user/user_bloc.dart';
 import 'package:tms_driver/presentation/pages/main/widget/main_view.dart';
@@ -25,6 +26,10 @@ class MainPage extends StatelessWidget {
         ),
         BlocProvider(
           create: (context) => UserBloc()..add(const UserEvent.started()),
+        ),
+        BlocProvider(
+          create: (context) =>
+              NotificationBloc()..add(const NotificationEvent.started()),
         ),
       ],
       child: const MainView(),
