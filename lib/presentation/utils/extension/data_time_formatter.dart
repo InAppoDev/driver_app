44fc4991e05466843919c252b10e4d370e0a3770
Valerii Kostinskiy <valerii.kostynskyi@gmail.com>
2024-09-06 10:47:20 +0300

    remove less code

--- lib/presentation/utils/extension/data_time_formatter.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-extension DateTimeFormatter on DateTime {
-  String to12HourFormat() {
-    DateFormat formatter = DateFormat('hh:mm a');
-    return formatter.format(this);
-  }
-}
