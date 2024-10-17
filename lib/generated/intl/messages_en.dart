5102891c646446c174b402b9855b719429581846
b1gmax <shovkoplyasmaks02@gmail.com>
2024-10-17 12:26:49 +0300

    fix

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -20,14 +20,23 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'en';
 
+  static String m0(distance) => "${distance}km";
+
+  static String m1(message) => "Error: ${message}";
+
+  static String m2(miles) => "Total: ${miles} miles";
+
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
         "accountInformation":
             MessageLookupByLibrary.simpleMessage("Account Information"),
+        "addComment": MessageLookupByLibrary.simpleMessage("add comment"),
         "address": MessageLookupByLibrary.simpleMessage("Address"),
         "allStops": MessageLookupByLibrary.simpleMessage("All Stops"),
         "approvedTrip": MessageLookupByLibrary.simpleMessage("Approved trip"),
+        "apptTime": MessageLookupByLibrary.simpleMessage("Appt/time"),
         "averageSpeed": MessageLookupByLibrary.simpleMessage("Average Speed"),
+        "begin": MessageLookupByLibrary.simpleMessage("begin trip"),
         "belowIsAShortSummaryOfYourMonth": MessageLookupByLibrary.simpleMessage(
             "Below is a short summary of your month"),
         "bolDocument": MessageLookupByLibrary.simpleMessage("BOL document:"),
@@ -37,16 +46,25 @@ class MessageLookup extends MessageLookupByLibrary {
         "checkCalls": MessageLookupByLibrary.simpleMessage("Check Calls"),
         "city": MessageLookupByLibrary.simpleMessage("City"),
         "commodity": MessageLookupByLibrary.simpleMessage("Commodity:"),
-        "confirmThatYouHaveEmbarkedOnThisTrip":
-            MessageLookupByLibrary.simpleMessage(
-                "Confirm that you have embarked on this trip:"),
+        "confirm": MessageLookupByLibrary.simpleMessage("confirm"),
+        "currentStop": MessageLookupByLibrary.simpleMessage("Current Stop"),
+        "custom": MessageLookupByLibrary.simpleMessage("Custom"),
+        "dismiss": MessageLookupByLibrary.simpleMessage("DISMISS"),
+        "distanceKm": m0,
         "driveApp": MessageLookupByLibrary.simpleMessage("DRIVER APP"),
         "driveOn": MessageLookupByLibrary.simpleMessage("Drive on"),
         "driveTime": MessageLookupByLibrary.simpleMessage("Drive Time"),
+        "editETA": MessageLookupByLibrary.simpleMessage("Edit ETA:"),
+        "emergencyPhone":
+            MessageLookupByLibrary.simpleMessage("Emergency Phone"),
         "enterCode": MessageLookupByLibrary.simpleMessage("Enter Code"),
         "enterYourPhoneNumberToLoginAnAccount":
             MessageLookupByLibrary.simpleMessage(
                 "Enter your Phone number to login an account"),
+        "errorMessage": m1,
+        "eta": MessageLookupByLibrary.simpleMessage("eta"),
+        "failedToLoadTrip": MessageLookupByLibrary.simpleMessage(
+            "Failed to load trip information. Please try again later or contact support."),
         "getNotifiedWhenWorkHappens": MessageLookupByLibrary.simpleMessage(
             "Get notified when work happens"),
         "getStarted": MessageLookupByLibrary.simpleMessage("Get Started"),
@@ -54,7 +72,9 @@ class MessageLookup extends MessageLookupByLibrary {
         "hereIsASummaryOfYourOverallPerformance":
             MessageLookupByLibrary.simpleMessage(
                 "Here is a summary of your overall\nperformance"),
+        "hideStops": MessageLookupByLibrary.simpleMessage("Hide Stops"),
         "home": MessageLookupByLibrary.simpleMessage("Home"),
+        "hour": MessageLookupByLibrary.simpleMessage("hour"),
         "hoursOfOperation":
             MessageLookupByLibrary.simpleMessage("Hours of operation:"),
         "initializing": MessageLookupByLibrary.simpleMessage("Initializing..."),
@@ -67,13 +87,18 @@ class MessageLookup extends MessageLookupByLibrary {
         "m": MessageLookupByLibrary.simpleMessage("M"),
         "messages": MessageLookupByLibrary.simpleMessage("Messages"),
         "milesToGo": MessageLookupByLibrary.simpleMessage("Miles to go"),
+        "min": MessageLookupByLibrary.simpleMessage("min"),
         "mph": MessageLookupByLibrary.simpleMessage("Mph"),
         "next": MessageLookupByLibrary.simpleMessage("NEXT"),
+        "nextStop": MessageLookupByLibrary.simpleMessage("next stop"),
         "or": MessageLookupByLibrary.simpleMessage("or"),
         "pallets": MessageLookupByLibrary.simpleMessage("Pallets:"),
+        "permissions": MessageLookupByLibrary.simpleMessage("Permissions"),
         "phone": MessageLookupByLibrary.simpleMessage("Phone"),
         "pickUp": MessageLookupByLibrary.simpleMessage("Pick up #2133123"),
         "profitDetails": MessageLookupByLibrary.simpleMessage("Profit Details"),
+        "readyToBegin":
+            MessageLookupByLibrary.simpleMessage("Ready to begin this trip?"),
         "rememberToKeepTrackOfYourProfessionalAccomplishments":
             MessageLookupByLibrary.simpleMessage(
                 "Remember to keep track of your professional accomplishments."),
@@ -85,10 +110,11 @@ class MessageLookup extends MessageLookupByLibrary {
             MessageLookupByLibrary.simpleMessage("Select ETA at PICK UP#1:"),
         "selectTheDateAndTimeOfArrivalAt": MessageLookupByLibrary.simpleMessage(
             "Select the date and time of arrival at PICK UP #1:"),
+        "setValue": MessageLookupByLibrary.simpleMessage("Set Value"),
         "shortestTrip": MessageLookupByLibrary.simpleMessage("Shortest Trip"),
         "skip": MessageLookupByLibrary.simpleMessage("SKIP"),
         "start": MessageLookupByLibrary.simpleMessage("START"),
-        "startingPoint": MessageLookupByLibrary.simpleMessage("Starting point"),
+        "startDriving": MessageLookupByLibrary.simpleMessage("Start driving"),
         "stateCode": MessageLookupByLibrary.simpleMessage("State Code"),
         "stayOrganizedWithTeam":
             MessageLookupByLibrary.simpleMessage("Stay organized with team"),
@@ -98,6 +124,7 @@ class MessageLookup extends MessageLookupByLibrary {
         "temperature": MessageLookupByLibrary.simpleMessage("Temperature:"),
         "tons": MessageLookupByLibrary.simpleMessage("Tons:"),
         "totalMiles": MessageLookupByLibrary.simpleMessage("Total Miles"),
+        "totalMilesMiles": m2,
         "totalStops": MessageLookupByLibrary.simpleMessage("Total Stops"),
         "totalTrips": MessageLookupByLibrary.simpleMessage("Total trips"),
         "trackYourWorkAndGetTheResult": MessageLookupByLibrary.simpleMessage(
@@ -114,6 +141,9 @@ class MessageLookup extends MessageLookupByLibrary {
         "verifyCode": MessageLookupByLibrary.simpleMessage("Verify Code"),
         "weight": MessageLookupByLibrary.simpleMessage("WEIGHT:"),
         "you": MessageLookupByLibrary.simpleMessage("You"),
+        "youHaveNoMessagesYetPullToRefresh":
+            MessageLookupByLibrary.simpleMessage(
+                "You have no messages yet, pull to refresh the page"),
         "youHaventYetStartedTheTrip": MessageLookupByLibrary.simpleMessage(
             "You haven`t yet started the trip."),
         "yourDataIsProtected": MessageLookupByLibrary.simpleMessage(
