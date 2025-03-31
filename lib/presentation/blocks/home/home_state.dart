a160ca58e7b2225f9bb3ac0631fd61b336148195
Valerii Kostinskiy <valerii.kostynskyi@gmail.com>
2025-03-31 16:52:41 +0300

    first

--- lib/presentation/blocks/home/home_state.dart
+++ lib/presentation/blocks/home/home_state.dart
@@ -1,9 +1,7 @@
 part of 'home_bloc.dart';
 
-
 @freezed
 class HomeState with _$HomeState {
-
   const factory HomeState.fetched(PersonalStatsModel? personalStats) = Fetched;
   const factory HomeState.loading() = Loading;
 }
